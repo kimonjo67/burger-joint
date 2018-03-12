febe11e935b56eff7339731110c9129ae663d171
kimonjo67 <monjokim@gmail.com>
2018-03-11 19:48:57 -0500

    server up and functioning

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+// I wonder if __dirname is needed here
+app.use(express.static(__dirname + "public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html" }));
+
+//override with POST having ?_method=DELETE or PUT
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them. Reference routes.js file
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
