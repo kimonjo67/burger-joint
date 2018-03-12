fb859c468c22ec08199cdb6e9093d082d56845a9
kimonjo67 <monjokim@gmail.com>
2018-03-11 19:42:43 -0500

    sql and seed added

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheese Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Taco Chicken Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Veggie Burger', FALSE);
+
